011d3191a743a8d69d5d09bb961ac5ce0bf9b817
deep0072 <deepakumar50136@gmail.com>
2022-10-15 13:57:13 +0530

    assignment2

--- jsAssignMent2/jsAssignMent.js
+++ jsAssignMent2/jsAssignMent.js
@@ -1,65 +1,65 @@
-// /* check whether a triangle is equilateral,
-// isosceles or scalene*/
+/* check whether a triangle is equilateral,
+isosceles or scalene*/
 
-// const { INSPECT_MAX_BYTES } = require("buffer");
+const { INSPECT_MAX_BYTES } = require("buffer");
 
-// function checkTriangle(x, y, z) {
-//   if (x === y && y === z) {
-//     console("equilateral triangle");
-//   } else if (x == y || y === z || x === z) {
-//     console.log("isosceles");
-//   } else {
-//     console.log("scalene");
-//   }
-// }
+function checkTriangle(x, y, z) {
+  if (x === y && y === z) {
+    console("equilateral triangle");
+  } else if (x == y || y === z || x === z) {
+    console.log("isosceles");
+  } else {
+    console.log("scalene");
+  }
+}
 
-// // checkTriangle(5, 6, 9);
+// checkTriangle(5, 6, 9);
 
-// // check grade
-// x = "46";
-// switch (true) {
-//   case 90 < x && x < 100:
-//     console.log("S grade");
-//     break;
+// check grade
+x = "46";
+switch (true) {
+  case 90 < x && x < 100:
+    console.log("S grade");
+    break;
 
-//   case 80 < x && x < 90:
-//     console.log(" A grade");
-//     break;
+  case 80 < x && x < 90:
+    console.log(" A grade");
+    break;
 
-//   case 70 < x && x < 80:
-//     console.log(" B grade");
-//     break;
+  case 70 < x && x < 80:
+    console.log(" B grade");
+    break;
 
-//   case 60 < x && x < 70:
-//     console.log(" C grade");
-//     break;
+  case 60 < x && x < 70:
+    console.log(" C grade");
+    break;
 
-//   case 50 < x && x < 60:
-//     console.log(" D grade");
-//     break;
+  case 50 < x && x < 60:
+    console.log(" D grade");
+    break;
 
-//   case 40 < x && x < 50:
-//     console.log(" E grade");
-//     break;
+  case 40 < x && x < 50:
+    console.log(" E grade");
+    break;
 
-//   case 0 < x && x < 40:
-//     console.log(" Student has failed");
-//     break;
-//   default:
-//     console.log("invalid marks");
-// }
+  case 0 < x && x < 40:
+    console.log(" Student has failed");
+    break;
+  default:
+    console.log("invalid marks");
+}
 
-// // sum of multiple of 3 and 5
-// j = 0;
-// x = 0;
-// while (x <= 1000) {
-//   if (x % 3 == 0 && x % 5 == 0) {
-//     j += x;
-//   }
-//   x++;
-// }
+// sum of multiple of 3 and 5
+j = 0;
+x = 0;
+while (x <= 1000) {
+  if (x % 3 == 0 && x % 5 == 0) {
+    j += x;
+  }
+  x++;
+}
 
-// console.log(j, x);
+console.log(j, x);
 
 // find factorial of all prime numbers between given range
 
